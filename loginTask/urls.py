e53a88a3cddcf2f6eb324d25dd40d3c1ab6cd671
Mirian503 <161772582+Mirian503@users.noreply.github.com>
2024-04-17 19:45:26 -1200

    Delete loginTask/urls.py

--- loginTask/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
-    path('tasks/', include('tasks.urls')),
-    path('', include('account.urls')),   
-]
